f2244a304888360f834c7ad48ffac6f9d4521e54
daniel-kollanyi <daniel.kollanyi@falcon.io>
2021-05-02 20:02:06 +0200

    Initial commit

---
+++ src/main/java/com/daniel/automationhero/AutomationHeroApplication.java
@@ -0,0 +1,13 @@
+package com.daniel.automationhero;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomationHeroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutomationHeroApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/daniel/automationhero/AutomationHeroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daniel.automationhero;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AutomationHeroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
